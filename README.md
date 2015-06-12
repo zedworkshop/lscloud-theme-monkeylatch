341a428fc104b7b0f76762f4de89a613757071bf
Adam Campbell <adam@hotmeteor.com>
2015-06-12 13:36:12 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+Coastal
+=============
+
+### The default [LemonStand](https://lemonstand.com/) theme for [Clear Solutions](https://clearsolutions.ca/).
+
+Created by: [Adam Campbell](http://zedworkshop.com) ([@hotmeteor](http://twitter.com/hotmeteor))
+
+#### Quick Install
+
+1. Download this theme as a [zip file](https://github.com/decorepair/lscloud-theme-clearsolutions/archive/master.zip)
+2. Follow the instructions for [importing a theme](http://docs.lemonstand.com/article/69-how-to-import-a-theme)
---
+++ src/js/main.js
@@ -0,0 +1,166 @@
+'use strict';
+
+$(document).ready(function() {
+
+    // Product sort
+    $(document).on('change', 'select[data-sort-redirect]', function() {
+        var path = window.location.pathname + '?' + $(this).val(),
+            baseUrl = window.location.protocol + '//' + window.location.host;
+        window.location = baseUrl + path;
+    });
+
+    (function($) {
+        $(document).on("click", ".js-thumblist a", function(e) {
+            e.preventDefault();
+            var $el = $(this);
+            var target = $el.closest(".js-thumblist").data("target");
+            var $target = $(target);
+            var src = $el.attr("href");
+            $el.parent().addClass("selected").siblings().removeClass("selected");
+            $target.attr("src", src);
+        });
+    })(jQuery);
+
+    $(document).on("click", ".js-addtocart", function(e) {
+
+        e.preventDefault();
+
+        $(this).sendRequest("shop:onAddToCart", {
+            update: {
+                "#mini-cart": "shop-minicart",
+                "#product-page": "shop-product",
+                "#modal-minicart": "modal-minicart"
+            },
+            onAfterUpdate: function() {
+                // $("#modal-minicart").modal({
+                //     backdrop: false,
+                //     show: true
+                // });
+            }
+        });
+    });
+
+    // $("#modal-minicart").on("mouseleave", ".modal-dialog", function() {
+    //     $("#modal-minicart").modal("hide");
+    // });
+
+    (function($) {
+
+        var $source = $("#billing-info");
+        var $sourceInput = $source.find(":input:not([type=hidden])");
+        var $target = $("#shipping-info");
+        var $targetInput = $target.find(":input:not([type=hidden])");
+
+        var $chk = $(document).find('.js-mirrordata');
+
+        var _regex = /\[(.*?)\]/;
+        var _ev = "keyup blur change";
+
+        // Update vars.
+        $(window).on('onAfterAjaxUpdate', function() {
+            $source = $("#billing-info");
+            $sourceInput = $source.find(":input:not([type=hidden])");
+            $target = $("#shipping-info");
+            $targetInput = $target.find(":input:not([type=hidden])");
+
+            $targetInput.prop('disabled', $chk.is(':checked'));
+        });
+
+        // Update on typing.
+        $(document).on(_ev, $sourceInput, function(ev) {
+
+            $chk = $(document).find('.js-mirrordata');
+
+            if (!$chk.is(':checked')) {
+                return;
+            }
+
+            mirrorField.apply(ev.target);
+
+            return false;
+        });
+
+        $(document).on('change', $chk, function() {
+            $targetInput.prop('disabled', $chk.is(':checked'));
+        });
+
+        $targetInput.prop('disabled', $chk.is(':checked'));
+
+
+        function mirrorField() {
+
+            if (!$(this).attr("name")) {
+                return;
+            }
+
+            var $el = $(this);
+            var mirrorVal = $el.val();
+            var nameMatch = $el.attr("name").match(_regex);
+
+            // $target = $("#shipping-info");
+            // $targetInput = $target.find(":input:not([type=hidden])");
+
+            if (!nameMatch) {
+                return;
+            }
+
+            nameMatch = nameMatch[1];
+
+            var re = new RegExp(nameMatch, "g");
+            var $targetEl = $targetInput.filter(function() {
+                return this.name.match(re);
+            });
+
+            $targetEl.val(mirrorVal);
+
+            if ($el[0].id === "billing_country") {
+                $targetEl.trigger("change");
+            }
+        }
+
+    })(jQuery);
+
+    $("#cart-content").on("keydown", "input#coupon", function(ev) {
+        if (ev.which === 13) {
+            $(this).sendRequest("shop:cart", {
+                update: {
+                    "#cart-content": "shop-cart-content",
+                    "#mini-cart": "shop-minicart"
+                },
+                extraFields: {
+                    set_coupon_code: 1
+                }
+            });
+        }
+    });
+
+    $("#cart-content").on("keydown", "input.quantity", function(ev) {
+        if (ev.which === 13) {
+            $(this).sendRequest("shop:cart", {
+                update: {
+                    "#cart-content": "shop-cart-content",
+                    "#mini-cart": "shop-minicart"
+                }
+            });
+        }
+    });
+
+    $("#checkout-page").on("change", "#shipping-methods input", function() {
+        $(this).sendRequest("shop:onCheckoutShippingMethod", {
+            update: {
+                "#checkout-totals": "shop-checkout-totals",
+                "#mini-cart": "shop-minicart"
+            }
+        });
+    });
+
+    $(document).on("change", "#payment_method input", function() {
+        $("#payment_form").html('<i class="fa fa-refresh fa-spin"/>');
+        $(this).sendRequest("shop:onUpdatePaymentMethod", {
+            update: {
+                "#payment_form": "shop-paymentform"
+            }
+        });
+    });
+
+});
